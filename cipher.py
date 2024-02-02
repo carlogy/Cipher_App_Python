b88bd1e4a0ce55e854311237d9092c93d19010e8
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-02-02 07:38:33 -0500

    set up loop to iterate through each character of a string and find the index of the string.

--- cipher.py
+++ cipher.py
@@ -1,2 +1,8 @@
-text = "Hello World"
-print(text)
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    ew_index = index + shift
