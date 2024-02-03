22ffbd03a6935a4e3aa7cccb9616c41c1c3654ce
carlogyannuzzi <carlogyannuzzi@gmail.com>
2024-02-03 12:46:05 +0000

    added for loop to iterate through each character in the text string to get it's index value and assing a new one.

--- cipher.py
+++ cipher.py
@@ -1,8 +1,10 @@
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
+encrypted_text = ''
 for char in text.lower():
     index = alphabet.find(char)
-    print(char, index)
-    ew_index = index + shift
+    new_index = index + shift
+    new_char =alphabet[new_index]
+    print('char:', char, 'new char:', new_char)
+
