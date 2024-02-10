03d11437deb109ef738a3d82198fa75a964b5fba
carlogyannuzzi <carlogyannuzzi@gmail.com>
2024-02-10 13:00:16 +0000

    updated function definition and some of the encryption logic.

--- cipher.py
+++ cipher.py
@@ -1,12 +1,17 @@
 text = 'Hello World'
-shift = 3
-def caesar(message, offset):
+custom_key = 'python'
+def vigenere(message, key):
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
     for char in message.lower():
+        # Append space to the message
         if char == ' ':
             encrypted_text += char
         else:
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
